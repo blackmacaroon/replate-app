940497bfc274d8cae0e92f66cec1764fa1ee7537
Kayla <6lackmacaroon@gmail.com>
2019-06-29 19:35:45 -0600

    created react app

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ server.use(express.json());
 
 //sanity check
 server.get('/', (req, res) => {
-      res.status(200).json({ message: 'look what you did, you little jerk.' });
+      res.status(200).json({ message: 'hello, world.' });
 });
 
 module.exports = server;
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
 
@@ -31,20 +31,20 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'postgresql',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    }
+  }
 
 };
