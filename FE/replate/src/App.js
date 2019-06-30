6e1ada40734644d5efad2c0140307433f3a34d1d
Kayla <6lackmacaroon@gmail.com>
2019-06-29 20:18:04 -0600

    building scaffolding for react app

--- FE/replate/src/App.js
+++ FE/replate/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import HomeView from './views/'; //finish routing, nothing in views yet
+import Register from './';
+import LoginGo from './';
+import LoginGet from './';
+import LoginGive from './';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div>
+            <HomeView />
+          </div>
+          <div>
+            <Route path='/register' component={Register} />
+            <Route path='/login/give' component={LoginGive}/>
+            <Route path='/login/go' component={LoginGo}/>
+            <Route path='/login/get' component={LoginGet}/>
+          </div>
+          
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- FE/replate/src/index.js
+++ FE/replate/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import thunk from 'redux-thunk';
 import ReactDOM from 'react-dom';
+import { reducer } from './reducers';   //not done routing, nothing in reducers yet
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk));
+window.reduxStore = store;
+
+ReactDOM.render(
+      <Provider store={store}>
+            <App />
+      </Provider>, 
+document.getElementById('root'));
 
